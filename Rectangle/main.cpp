1dffae73feda0305492cb70891c93c6d91b5d38f
Masoud Darvishian <masoud.darvishian@etermax.de>
2023-10-01 12:25:12 +0200

    Added completed script for the rectanble/hexagon

--- Rectangle/main.cpp
+++ Rectangle/main.cpp
@@ -14,7 +14,7 @@ int main()
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(640, 480, "Hello, Triangle!", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(640, 550, "Hello, Hexagon!", NULL, NULL);
 	if (!window)
 	{
 		std::cout << "Failed to create the window!" << std::endl;
@@ -83,24 +83,39 @@ int main()
 	glDeleteShader(fragmentShader);
 
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		 0.0f,  0.5f, 0.0f,
-		 0.5f, -0.5f, 0.0f,
+		 0.3f,  0.5f, 0.0f,  // top right
+		 0.5f,  0.0f, 0.0f,  // middle right
+		 0.3f, -0.5f, 0.0f,  // bottom right
+		-0.3f, -0.5f, 0.0f,  // bottom left
+		-0.5f,  0.0f, 0.0f,  // middle left
+		-0.3f,  0.5f, 0.0f   // top left 
+	};
+	unsigned int indices[] = {
+		0, 5, 4, // first triangle
+		0, 4, 1, // second triangle
+		1, 4, 3, // third triangle
+		1, 3, 2  // forth triangle
 	};
 
-	unsigned int VAO, VBO;
+	unsigned int VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glBindVertexArray(VAO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 3, (void*)0);
 	glEnableVertexAttribArray(0);
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -111,7 +126,7 @@ int main()
 
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
 		glfwSwapBuffers(window);
 
